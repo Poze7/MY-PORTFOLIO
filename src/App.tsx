6c2c86029add9de407165abe089afcdab1f24a77
Poze7 <matheus.caetano@estudante.ifms.edu.br>
2023-08-25 08:02:57 -0400

    fds

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
+import Main from './componentes/main/Main'
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div>
     <Header/>
     <Footer/>
+    <Main/>
       </div>
   )
 }
