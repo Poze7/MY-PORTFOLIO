c9b7fab6e19cb3e3a9338d849c1dfe1be1fb3277
Poze7 <matheus.caetano@estudante.ifms.edu.br>
2023-09-01 09:14:38 -0400

    fdss

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,6 +1,8 @@
 import './Main.css';
 
 export default function Main() {
+
+
   return (
     <div className="main-container">
       <div className="content-container">
@@ -9,13 +11,12 @@ export default function Main() {
         <h2 className="main2-2">Estudante do IFMS</h2>
         <h2 className="main2-3"> Campus de Naviraí</h2>
         <h1 className="main1-2">Seja bem-vindo ao meu portfólio</h1>
-        <h1 className="main1-3">Me conheça melhor</h1>
+        <button className="main1-3" >
+          Me conheça melhor
+        </button>
       </div>
 
-  
-      <img src="main.png" alt="main" className='img2'></img>
-    
-
+      <img src="main.png" alt="main" className='img2' />
     </div>
   );
 }
