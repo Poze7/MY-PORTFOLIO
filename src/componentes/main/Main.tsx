f834c983604db88069650e12244791ba5867f4b1
Poze7 <matheus.caetano@estudante.ifms.edu.br>
2023-09-19 08:07:54 -0400

    fds

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -4,6 +4,9 @@ export default function Main() {
 
 
   return (
+    <body>
+      
+    
     <div className="main-container">
       <div className="content-container">
         <h1 className="main1-1">Oi, eu sou </h1>
@@ -15,8 +18,36 @@ export default function Main() {
           Me conheça melhor
         </button>
       </div>
+      <div className="search-bar">
+  <input
+    type="text"
+    placeholder="Pesquisar projetos"
+    className="barra"
+  />
+  <button className="pesquisa">Pesquisar</button>
+</div>
+<div className="projetos">
+  <h2 className="projeto-h2">Meus Projetos</h2>
+  <div className="projeto-lista">
+    {/* Aqui você pode adicionar a lista de projetos */}
+    <div className="projeto-card">
+      <h3>Projeto 1</h3>
+      <p>Descrição do Projeto 1...</p>
+      {/* Adicione mais informações sobre o Projeto 1 */}
+    </div>
+    <div className="projeto-card">
+      <h3>Projeto 2</h3>
+      <p>Descrição do Projeto 2...</p>
+      {/* Adicione mais informações sobre o Projeto 2 */}
+    </div>
+    {/* Adicione mais cartões de projeto conforme necessário */}
+  </div>
+</div>
+
+
 
       <img src="main.png" alt="main" className='img2' />
     </div>
+    </body>
   );
 }
